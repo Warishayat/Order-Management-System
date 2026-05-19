c36c1da0233e9938d203a4961c2818736bbf81c8
waris hayat <warishayat666@gmail.com>
2026-05-19 21:18:06 +0500

    Frontend issues fixed

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-  baseURL: 'https://order-management-system-srae.onrender.com', 
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000', 
 });
 api.interceptors.request.use(
   (config) => {
